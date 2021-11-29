39066864573faaf38e43502cf280b68263180044
Anjacodes <anja_schmidt7@posteo.de>
2021-11-29 17:00:23 -0600

    Create JavaScript Function to Dynamically Create List Elements, Style Task List

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('ul');
+const tasks = [{description: 'Water plants', completed: false, index: 0}, {description: 'Feed the dog', completed: false, index: 1},{description: 'Do homework', completed: false, index: 2}];
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], '');
-    return element;
+const createLi = () => {
+    const taskContainer = document.getElementById('taskContainer');
+    tasks.forEach(task => {
+        let li = document.createElement('li');
+        li.classList.add('listEl');
+        let input = document.createElement('input');
+        let label = document.createElement('label');
+        input.setAttribute('type', 'checkbox');
+        label.textContent = task.description;
+        li.appendChild(input);
+        li.appendChild(label);
+        taskContainer.appendChild(li);
+    });
 }
 
-document.body.appendChild(component());
+window.addEventListener('load', createLi);
