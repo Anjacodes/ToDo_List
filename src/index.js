a57929fa5d4da88be872f4fb7864abbbf98c0e1d
Anjacodes <anja_schmidt7@posteo.de>
2021-11-30 09:06:58 -0600

    Fix Linter Errors

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import _ from 'lodash';
+import _ from 'lodash'; // eslint-disable-line
 import './style.css';
 
-const tasks = [{description: 'Water plants', completed: false, index: 0}, {description: 'Feed the dog', completed: false, index: 1},{description: 'Do homework', completed: false, index: 2}];
+const tasks = [{ description: 'Water plants', completed: false, index: 0 }, { description: 'Feed the dog', completed: false, index: 1 }, { description: 'Do homework', completed: false, index: 2 }];
 
 const createLi = () => {
-    const taskContainer = document.getElementById('taskContainer');
-    tasks.forEach(task => {
-        let li = document.createElement('li');
-        li.classList.add('listEl');
-        let input = document.createElement('input');
-        let label = document.createElement('label');
-        input.setAttribute('type', 'checkbox');
-        label.textContent = task.description;
-        li.appendChild(input);
-        li.appendChild(label);
-        taskContainer.appendChild(li);
-    });
-    const clearBtn = document.createElement('button');
-    clearBtn.setAttribute('type', 'button');
-    clearBtn.classList.add('clearBtn');
-    clearBtn.textContent = 'Clear all completed'
-    taskContainer.appendChild(clearBtn);
-}
+  const taskContainer = document.getElementById('taskContainer');
+  tasks.forEach((task) => {
+    const li = document.createElement('li');
+    li.classList.add('listEl');
+    const input = document.createElement('input');
+    const label = document.createElement('label');
+    input.setAttribute('type', 'checkbox');
+    label.textContent = task.description;
+    li.appendChild(input);
+    li.appendChild(label);
+    taskContainer.appendChild(li);
+  });
+  const clearBtn = document.createElement('button');
+  clearBtn.setAttribute('type', 'button');
+  clearBtn.classList.add('clearBtn');
+  clearBtn.textContent = 'Clear all completed';
+  taskContainer.appendChild(clearBtn);
+};
 
 window.addEventListener('load', createLi);
--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,26 @@ const path = require('path');
 const HmtlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    devServer: {
-        static: './dist',
-    },
-    plugins: [
-        new HmtlWebpackPlugin({
-            title: 'Output Management',
-            template: './src/index.html'
-        }),
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HmtlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-           {
-               test: /\.css$/i,
-               use: ['style-loader', 'css-loader'],
-           },
-        ],
-    },
+  },
 };
